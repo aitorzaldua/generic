a68db9ed9ea3bc6b33e501ac6bf4ca356dc3ff06
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-09-18 20:20:52 +0200

    hero 1

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3005;
+const publicPath = path.join(__dirname, '..', 'generic/public');
+const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
+   res.send('mensaje cambiado a las 20:19');
    res.sendFile(path.join(publicPath, 'index.html'));
 });
 app.listen(port, () => {
-   console.log('Servidor generic levantado');
+   console.log('Servidor generic levantado 20:19');
 });
